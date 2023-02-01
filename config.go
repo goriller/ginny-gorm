57ce309d976c6e552611a97bd4edacd10d224c98
linyyyang <linyyyang@tencent.com>
2023-02-01 11:09:44 +0800

    fix: 统一dsn格式

--- config.go
+++ config.go
@@ -1,6 +1,10 @@
 package orm
 
 import (
+	"fmt"
+	"net/url"
+	"strings"
+
 	"github.com/google/wire"
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
@@ -14,7 +18,7 @@ var ConfigProvider = wire.NewSet(NewConfig)
 // Config
 type Config struct {
 	Type string `json:"type" mapstructure:"type"`
-	// [user[:password]@][net[(addr)]]/dbname[?param1=value1&paramN=valueN]
+	// mysql://localhost:3306/dbname[?username=value1&password=value2&paramN=valueN]
 	WDB  string   `json:"wdb" mapstructure:"wdb"`
 	RDBs []string `json:"rdbs" mapstructure:"rdbs"`
 	gorm.Config
@@ -36,5 +40,41 @@ func NewConfig(v *viper.Viper, logger *zap.Logger) (*Config, error) {
 			o.WDB,
 		}
 	}
+
+	// parse dsn
+	o.WDB, err = parseUrl(o.WDB)
+	if err != nil {
+		return nil, errors.Wrap(err, "parse dsn error")
+	}
+
+	rdb := []string{}
+	for _, v := range o.RDBs {
+		u, err := parseUrl(v)
+		if err != nil {
+			return nil, errors.Wrap(err, "parse dsn error")
+		}
+		rdb = append(rdb, u)
+	}
+	o.RDBs = rdb
+
 	return o, nil
+}
+
+func parseUrl(dsn string) (string, error) {
+	if dsn == "" {
+		return "", fmt.Errorf("undefined wdb dsn")
+	}
+	u, err := url.Parse(dsn)
+	if err != nil {
+		return "", err
+	}
+	q := u.Query()
+	wdbUser := q.Get("username")
+	wdbPass := q.Get("password")
+
+	q.Del("username")
+	q.Del("password")
+
+	// [user[:password]@][net[(addr)]]/dbname[?param1=value1&paramN=valueN]
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s?%s", wdbUser, wdbPass, u.Host, strings.TrimPrefix(u.Path, "/"), q.Encode()), nil
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/goriller/ginny-util/graceful v0.0.0-20221027062115-fdafa89a4dde
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/viper v1.14.0
+	github.com/stretchr/testify v1.8.1
 	go.uber.org/zap v1.24.0
 	gorm.io/driver/clickhouse v0.5.0
 	gorm.io/driver/mysql v1.4.5
@@ -21,6 +22,7 @@ require (
 	github.com/ClickHouse/ch-go v0.48.0 // indirect
 	github.com/ClickHouse/clickhouse-go/v2 v2.3.0 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-faster/city v1.0.1 // indirect
 	github.com/go-faster/errors v0.6.1 // indirect
@@ -44,6 +46,7 @@ require (
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/pierrec/lz4/v4 v4.1.17 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/spf13/afero v1.9.2 // indirect
